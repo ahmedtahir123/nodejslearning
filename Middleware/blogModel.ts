267a1c4a955cca9102cf7da44967548b6636b0eb
Ahmed Muhammad <amuhammad@nisum.com>
2024-01-01 17:20:17 +0500

    discussion resolve

--- Middleware/blogModel.ts
+++ Middleware/blogModel.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 
-export const blobModel = (req: Request, res: Response, next: NextFunction) => {
+export function blobModelMiddleWare (req: Request, res: Response, next: NextFunction) {
     const { title, content, username } = req.body;
-  if (title !== null &&  content!== null && username !== null ) {
-    return res.status(500).send('VAlue is missing');
-  }
-  next();
-};
+    if (title !== null &&  content!== null && username !== null ) {
+      return res.status(500).send('VAlue is missing');
+    }
+    next();
+}
