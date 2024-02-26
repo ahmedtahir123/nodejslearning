c06137a10f5b8bbb3be2879ad73c63b369a1b202
Ahmed Muhammad <amuhammad@nisum.com>
2024-02-26 17:09:04 +0500

    add pm2

--- app.ts
+++ app.ts
@@ -3,16 +3,24 @@ import express from 'express'
 const app = express()
 const PORT = 4000
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
-
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
 
 app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
+})
+
+app.get('/loadTest', (req, res) => {
+  let total = 0
+  for (let index = 0; index < 5000000; index++) {
+    total = index;
+  }
+  res.send(`The result of CPU intensive task is ${total}`)
+})
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
 })
 
 // Export the Express API
--- dist/app.js
+++ dist/app.js
@@ -6,14 +6,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = 4000;
-app.listen(PORT, () => {
-    console.log(`API listening on PORT ${PORT} `);
-});
 app.get('/', (req, res) => {
     res.send('Hey this is my API running 🥳');
 });
 app.get('/about', (req, res) => {
     res.send('This is my about route..... ');
+});
+app.get('/loadTest', (req, res) => {
+    let total = 0;
+    for (let index = 0; index < 5000000; index++) {
+        total = index;
+    }
+    res.send(`The result of CPU intensive task is ${total}`);
+});
+app.listen(PORT, () => {
+    console.log(`API listening on PORT ${PORT} `);
 });
 // Export the Express API
 module.exports = app;
